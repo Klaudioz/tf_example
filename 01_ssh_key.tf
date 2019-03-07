e6fb04aebe29ac12b102c9ec5b6e18f23cbe7426
Claudio Canales <ccanales@progrexion.com>
2019-03-06 19:28:42 -0700

    Moved documentation link

--- 01_ssh_key.tf
+++ 01_ssh_key.tf
@@ -1,7 +1,6 @@
 #
 # Exporting our SSH key
 # https://www.terraform.io/docs/providers/do/r/ssh_key.html
-# ENV VAR: https://www.terraform.io/docs/commands/environment-variables.html
 
 resource "digitalocean_ssh_key" "claudio" {
   name       = "claudio"
